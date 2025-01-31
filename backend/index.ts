940bbf0877c7a97ab3923eac6e2550f41dfd4983
PriyathikGit <techhack237@gmail.com>
2025-01-31 19:25:43 +0530

    Add CORS support and serve frontend files from backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,23 +1,24 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-import main from "./main";
-
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import main from './main';
+import path from 'path';
 dotenv.config();
 
 const app: Express = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the alloan.ai");
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Welcome to the alloan.ai");
+// });
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-app.use("/api", main);
-
+app.use('/api', main);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
